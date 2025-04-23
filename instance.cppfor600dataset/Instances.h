70f54b7a18a3a529ccb737a2884644fe0dcbfdf1
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2025-04-23 17:22:27 +0700

    Add files via upload

---
+++ instance.cppfor600dataset/Instances.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+class INSTANCE
+{
+public:
+    vector<vector<double>> tau;
+    vector<vector<double>> tauprime;
+    vector<vector<double>> nodes;
+    vector<int> Cprime;
+    vector<int> C;
+    vector<double> dDrones;
+    int UAVs;
+    int n;
+    bool loadFromFile(const std::string& filename = "20140813T111604.csv");
+    void displayData();
+};
+double tinhkc(const vector<vector<double>>& mat, int node);
