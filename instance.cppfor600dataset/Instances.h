b323ef923e49ebf483da27a6b5956a139b89eebb
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2025-06-13 22:31:57 +0700

    Update Instances.h

--- instance.cppfor600dataset/Instances.h
+++ instance.cppfor600dataset/Instances.h
@@ -13,8 +13,10 @@ public:
     vector<vector<double>> tauprime;
     vector<vector<double>> nodes;
     vector<int> Cprime;
+	string flename;
     vector<int> C;
     vector<double> dDrones;
+	vector<int> truckonly;
     int UAVs;
     int n;
     bool loadFromFile(const std::string& filename = "20140813T111604.csv");
